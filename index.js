6d9f4001fe97d6e53c56850fb5d4b5c5ee1d4b4c
Nathan Fritz <fritzy@netflint.net>
2014-07-22 22:07:42 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+var stream = require('stream');
+var util = require('util');
+
+function GetKeyOfValue (db, getopts) {
+    if (!this instanceof GetKeyOfValue) {
+        return new GetKeyOfValue(db, opts);
+    }
+    var opts = {};
+    opts.objectMode = true;
+    stream.Transform.call(this, opts);
+
+    this.db = db;
+    this.getopts = getopts;
+}
+
+util.inherits(GetKeyOfValue, stream.Transform);
+
+(function () {
+
+    this._transform = function (chunk, e, next) {
+        this.db.get(chunk.value, this.getopts, function (err, value) {
+            chunk.key = chunk.value;
+            chunk.value = value;
+            this.push(chunk);
+            next();
+        }.bind(this));
+    };
+
+}).call(GetKeyOfValue.prototype);
+
+module.exports = {
+    createPrefixReadStream: function (db, prefix, opts) {
+        opts = opts || {};
+        opts.start = opts.end = prefix;
+        if (opts.reverse) {
+            opts.start += '~';
+        } else {
+            opts.end += '~';
+        }
+        return db.createReadStream(opts);
+    },
+
+    GetKeyOfValue: GetKeyOfValue,
+};
